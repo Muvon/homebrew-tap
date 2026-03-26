56cfceaab27f07a4fe9d2a347cae4769cbb28425
Don Hardman <don.hardman@muvon.io>
2026-03-26 16:13:34 +0300

    ci: add automated octoweb cask update workflow

--- Casks/octoweb.rb
+++ Casks/octoweb.rb
@@ -7,12 +7,12 @@ class Octoweb < Cask
   on_macos do
     on_intel do
       url "https://github.com/muvon/octoweb/releases/download/#{version}/Octoweb-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "use_real_sha256_after_first_release"
+      sha256 "ae8458b1bc1b51dc132b3fb9aaadd559ca5d5be1f759f0409eceb0d2aa4794a1"
     end
 
     on_arm do
       url "https://github.com/muvon/octoweb/releases/download/#{version}/Octoweb-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "use_real_sha256_after_first_release"
+      sha256 "df0c0621fc69293f1a1c38ab26ad5db5a2cdc8262c763de4ca1f158578cc1bda"
     end
   end
 
