8d3617d4474223b1f0f2e194867e7334c8befd77
Don Karter <don@muvon.io>
2026-05-24 13:30:50 +0300

    chore(vext): bump version to 1.2.0

--- Casks/vext.rb
+++ Casks/vext.rb
@@ -1,6 +1,6 @@
 cask "vext" do
-  version "1.1.0"
-  sha256 "8a0672c7b62d366c36c0b353fd29800bb2f0e3c413bf8e62df26d737f62965b0"
+  version "1.2.0"
+  sha256 "4d64fde75bc31e03ab8b1c297885e73b5b275fac1b56e4552061ec02f505ca20"
 
   url "https://cdn.getvext.app/#{version.major}/Vext-#{version}.dmg"
   name "Vext"
