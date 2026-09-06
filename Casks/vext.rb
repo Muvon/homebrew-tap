f4d0eb74cddad09a2dd09295556ca407d878abbd
Don Karter <don@muvon.io>
2026-09-06 13:18:39 +0700

    chore(cask): update Vext to 1.5.1

--- Casks/vext.rb
+++ Casks/vext.rb
@@ -1,6 +1,6 @@
 cask "vext" do
-  version "1.5.0"
-  sha256 "b9dd58cdedff6e25555182fdaa32250651473b589bd6e6351ad4f8acbd66c18f"
+  version "1.5.1"
+  sha256 "f9336f49fc297fc141daed3ea997d531858e2054e8d79414e4281a115e055cbb"
 
   url "https://cdn.getvext.app/#{version.major}/Vext-#{version}.dmg"
   name "Vext"
